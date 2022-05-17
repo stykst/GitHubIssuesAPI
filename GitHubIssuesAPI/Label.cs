ff78be6169f6196883fb321556043f8a0ea3e575
stykst <k_stoyanov@alice.it>
2022-05-17 15:28:37 +0300

    Create Label.cs

---
+++ GitHubIssuesAPI/Label.cs
@@ -0,0 +1,13 @@
+namespace GitHubIssuesAPI.Test
+{
+    public class Label
+    {
+        public int id { get; set; }
+        public string node_id { get; set; }
+        public string url { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string color { get; set; }
+        public bool @default { get; set; }
+    }
+}
